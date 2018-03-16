816a5c2d1380871aa313f1b8d17691802f9b8165
Jarid11 <Marinos1995@gmail.com>
2018-03-15 21:46:23 -0500

    Jarid's Second Commit

--- app/src/components/Calculator/Calculator.js
+++ app/src/components/Calculator/Calculator.js
@@ -4,13 +4,32 @@ import calculatorImg from "../../calculator.png";
 class Calculator extends Component {
   constructor() {
     super();
+
+    this.state = {
+      header: "Calculator",
+      display: "0",
+      operator: "",
+      temp: 0,
+      resetDisplay: false
+    };
+
+    this.updateHeader = this.updateHeader.bind(this);
+  }
+
+  updateHeader(val) {
+    this.setState({
+      header: val
+    });
   }
 
   render() {
     return (
       <div id="calculator-container">
-        <input id="header-input" />
-        <h1 id="header"> Calculator </h1>
+        <input
+          id="header-input"
+          onChange={e => this.updateHeader(e.target.value)}
+        />
+        <h1 id="header"> {this.state.header} </h1>
         <img
           className="remove-highlight"
           src={calculatorImg}
